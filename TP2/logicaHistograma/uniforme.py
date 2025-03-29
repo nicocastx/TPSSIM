30a143f14004d5b7d27a17ae5ef5302323bae94d
nicocastx <nicocastx@gmail.com>
2025-03-29 17:17:35 -0300

    asfsdfbdsbf

--- TP2/logicaHistograma/uniforme.py
+++ TP2/logicaHistograma/uniforme.py
@@ -1,27 +1,60 @@
+import random
 import tkinter as tk
 from tkinter import ttk
+import matplotlib.pyplot as plt
+
 
 class Tabla(tk.Toplevel):
     def __init__(self, root):
+        self.serie = []
         super().__init__(root)
         print(root.campos[0], root.campos[1])
         self.title("Tabla de resultados")
         self.geometry("500x300")
 
+        #calcular la serie
+        for i in range(int(root.campos[0])):
+            rnd = random.random()
+            self.serie.append(rnd)
+
+        a = self.truncar(min(self.serie))
+        b = self.truncar(max(self.serie))
+        print(a)
+        print(b)
+
+        numIntervalos = int(root.campos[1])  # número de intervalos
+
+        # calcular el tamaño de cada intervalo
+        anchoIntervalo = (b - a) / numIntervalos
+        print(anchoIntervalo)
+
+        intervalos = []
+
+        for i in range(numIntervalos):
+            inicio = self.truncar(a)
+            fin = self.truncar(a + anchoIntervalo)
+            intervalos.append((inicio, fin))
+            a = fin
+        print("cantidad de intervalos: " + str(len(intervalos)))
+
+        print(intervalos)
+
+
+
         # Crear tabla
         self.tabla = ttk.Treeview(self)
-        self.tabla['columns'] = ('Columna 1', 'Columna 2', 'Columna 3')
+        self.tabla['columns'] = ('Intervalos', 'columna 2', 'Columna 3')
 
         # Formato de la tabla
         self.tabla.column("#0", width=0, stretch=tk.NO)
-        self.tabla.column("Columna 1", anchor=tk.W, width=100)
-        self.tabla.column("Columna 2", anchor=tk.W, width=100)
+        self.tabla.column("Intervalos", anchor=tk.W, width=100)
+        self.tabla.column("columna 2", anchor=tk.W, width=100)
         self.tabla.column("Columna 3", anchor=tk.W, width=100)
 
         # Encabezados de la tabla
         self.tabla.heading("#0", text="", anchor=tk.W)
-        self.tabla.heading("Columna 1", text="Columna 1", anchor=tk.W)
-        self.tabla.heading("Columna 2", text="Columna 2", anchor=tk.W)
+        self.tabla.heading("Intervalos", text="Intervalos", anchor=tk.W)
+        self.tabla.heading("columna 2", text="columna 2", anchor=tk.W)
         self.tabla.heading("Columna 3", text="Columna 3", anchor=tk.W)
 
         # Agregar datos a la tabla (por ejemplo, puedes agregar datos aquí)
@@ -34,6 +67,7 @@ class Tabla(tk.Toplevel):
 
         # Agregar tabla a la ventana
         self.tabla.pack()
+
 
     def copiar_valores(self):
         # Obtener la lista de ítems de la tabla
@@ -59,5 +93,14 @@ class Tabla(tk.Toplevel):
         self.clipboard_clear()
         self.clipboard_append(csv_data)
 
+        self.mostrar_histograma(self.serie, int(self.campos[1]))
+
         # Imprimir un mensaje para confirmar que los valores han sido copiados
         print("Valores copiados")
+
+    def truncar(num):
+        truncado = round(num, 4)
+        if truncado == 16:
+            return 2
+        else:
+            return int(truncado * 10000) / 10000
